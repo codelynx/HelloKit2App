8be68cdc102d176a0d4e73450bd3003c72cb6a49
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2022-09-01 15:41:00 +0900

    initial commit

---
+++ HelloKit2App/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  HelloKit2App
+//
+//  Created by 吉川 和成 on 9/1/22.
+//
+
+import UIKit
+import HelloKit2
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	let hello = HelloKit2.SwiftyHello()
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		hello.hello()
+		return true
+	}
+
+	// MARK: UISceneSession Lifecycle
+
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		// Called when a new scene session is being created.
+		// Use this method to select a configuration to create the new scene with.
+		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	}
+
+	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+		// Called when the user discards a scene session.
+		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	}
+
+
+}
+
---
+++ HelloKit2App/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HelloKit2App
+//
+//  Created by 吉川 和成 on 9/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+}
+
